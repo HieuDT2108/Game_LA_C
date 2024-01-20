dc7e2a3fa15728f3bbf18a62597232faa3b09d92
HieuDT2108 <79426353+HieuDT2108@users.noreply.github.com>
2024-01-20 09:19:19 +0700

    Update main ver.0.9.py

--- main.py
+++ main.py
@@ -1,38 +1,153 @@
-import random
-import math
-
-limit = 1000
-acc = 0
-results = []
-exp = 1000
+import os
 
 
-for i in range(exp):
-  color = 0
-  amount = 10000
-  max_amount = amount
-  bid = 1
-  count = 0
-  while count < limit and amount > 0 :
-    amount = amount - bid
-    next = random.randint(0, 1)
-    if next == color :
-      amount = amount + bid + bid
-      bid = 1
-      # color = 1 if color == 0 else 0
-      if amount > max_amount:
-        max_amount = amount
-    else :
-      bid = bid + bid
-    count = count + 1
-  acc = acc + max_amount
-  results.append(max_amount)
-  print("Exp {}".format(i))
+def addition():
+   os.system('cls' if os.name == 'nt' else 'clear')
+   print('Addition')
 
-avg = acc / exp
-acc = 0
-for i in range(len(results)):
-  acc = acc + math.pow(results[i] - avg, 2)
-std = math.sqrt(acc / exp)
+   continue_calc = 'y'
 
-print("Average max amount earned {} with standard deviation {}".format(avg, std))
+   num_1 = float(input('Enter a number: '))
+   num_2 = float(input('Enter another number: '))
+   ans = num_1 + num_2
+   values_entered = 2
+   print(f'Current result: {ans}')
+
+   while continue_calc.lower() == 'y':
+       continue_calc = (input('Enter more (y/n): '))
+       while continue_calc.lower() not in ['y', 'n']:
+           print('Please enter \'y\' or \'n\'')
+           continue_calc = (input('Enter more (y/n): '))
+
+       if continue_calc.lower() == 'n':
+           break
+       num = float(input('Enter another number: '))
+       ans += num
+       print(f'Current result: {ans}')
+       values_entered += 1
+   return [ans, values_entered]
+
+
+def subtraction():
+   os.system('cls' if os.name == 'nt' else 'clear')
+   print('Subtraction')
+
+   continue_calc = 'y'
+
+   num_1 = float(input('Enter a number: '))
+   num_2 = float(input('Enter another number: '))
+   ans = num_1 - num_2
+   values_entered = 2
+   print(f'Current result: {ans}')
+
+   while continue_calc.lower() == 'y':
+       continue_calc = (input('Enter more (y/n): '))
+       while continue_calc.lower() not in ['y', 'n']:
+           print('Please enter \'y\' or \'n\'')
+           continue_calc = (input('Enter more (y/n): '))
+
+       if continue_calc.lower() == 'n':
+           break
+       num = float(input('Enter another number: '))
+       ans -= num
+       print(f'Current result: {ans}')
+       values_entered += 1
+   return [ans, values_entered]
+
+
+def multiplication():
+   os.system('cls' if os.name == 'nt' else 'clear')
+   print('Multiplication')
+
+   continue_calc = 'y'
+
+   num_1 = float(input('Enter a number: '))
+   num_2 = float(input('Enter another number: '))
+   ans = num_1 * num_2
+   values_entered = 2
+   print(f'Current result: {ans}')
+
+   while continue_calc.lower() == 'y':
+       continue_calc = (input('Enter more (y/n): '))
+       while continue_calc.lower() not in ['y', 'n']:
+           print('Please enter \'y\' or \'n\'')
+           continue_calc = (input('Enter more (y/n): '))
+
+       if continue_calc.lower() == 'n':
+           break
+       num = float(input('Enter another number: '))
+       ans *= num
+       print(f'Current result: {ans}')
+       values_entered += 1
+   return [ans, values_entered]
+
+
+def division():
+   os.system('cls' if os.name == 'nt' else 'clear')
+   print('Division')
+
+   continue_calc = 'y'
+
+   num_1 = float(input('Enter a number: '))
+   num_2 = float(input('Enter another number: '))
+   while num_2 == 0.0:
+       print('Please enter a second number > 0')
+       num_2 = float(input('Enter another number: '))
+
+   ans = num_1 / num_2
+   values_entered = 2
+   print(f'Current result: {ans}')
+
+   while continue_calc.lower() == 'y':
+       continue_calc = (input('Enter more (y/n): '))
+       while continue_calc.lower() not in ['y', 'n']:
+           print('Please enter \'y\' or \'n\'')
+           continue_calc = (input('Enter more (y/n): '))
+
+       if continue_calc.lower() == 'n':
+           break
+       num = float(input('Enter another number: '))
+       while num == 0.0:
+           print('Please enter a number > 0')
+           num = float(input('Enter another number: '))
+       ans /= num
+       print(f'Current result: {ans}')
+       values_entered += 1
+   return [ans, values_entered]
+
+
+def calculator():
+   quit = False
+   while not quit:
+       results = []
+       print('Simple Calculator in Python!')
+       print('Enter \'a\' for addition')
+       print('Enter \'s\' for substraction')
+       print('Enter \'m\' for multiplication')
+       print('Enter \'d\' for division')
+       print('Enter \'q\' to quit')
+
+       choice = input('Selection: ')
+
+       if choice == 'q':
+           quit = True
+           continue
+
+       if choice == 'a':
+           results = addition()
+           print('Ans = ', results[0], ' total inputs: ', results[1])
+       elif choice == 's':
+           results = subtraction()
+           print('Ans = ', results[0], ' total inputs: ', results[1])
+       elif choice == 'm':
+           results = multiplication()
+           print('Ans = ', results[0], ' total inputs: ', results[1])
+       elif choice == 'd':
+           results = division()
+           print('Ans = ', results[0], ' total inputs: ', results[1])
+       else:
+           print('Sorry, invalid character')
+
+
+if __name__ == '__main__':
+   calculator()
